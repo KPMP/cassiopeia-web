53e0d470affa6687a3bad36646facd3799cfbe07
Zach Wright <zwright@umich.edu>
2018-11-16 12:16:45 -0500

    KPMP 423: Rename

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Nav/NavBar';
-import LandingPage from './components/LandingPage/LandingPage'
+import Summary from './components/Summary/Summary'
 
 class App extends Component {
   render() {
     return (
       <div >
       	<Navbar/>
-       	<LandingPage/>
+       	<Summary/>
       </div>
     );
   }
