c17b61c66872db566a35ca25f35e3d2be09d2b8d
Zach Wright <zwright@umich.edu>
2018-11-16 11:52:22 -0500

    KPMP 423: Patient ID selector

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/Nav/NavBar';
+import LandingPage from './components/LandingPage/LandingPage'
 
 class App extends Component {
   render() {
     return (
       <div >
       	<Navbar/>
-       	<h1>Welcome to the Patient Whole Slide Image Viewer!</h1>
+       	<LandingPage/>
       </div>
     );
   }
