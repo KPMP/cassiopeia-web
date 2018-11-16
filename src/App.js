b5a8b5d07690c1dedf5b3f744c6caf359da8c2a4
Zach Wright <zwright@umich.edu>
2018-11-16 13:20:20 -0500

    KPMP-426: Add slides page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Navbar from './components/Nav/NavBar';
-import Summary from './components/Summary/Summary'
+import Summary from './components/Summary/Summary';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import loadedState from './initialState';
+import rootReducer from './reducers';
+
+window.sessionStorage.clear();
+const cacheStore = window.sessionStorage.getItem("redux-store");
+const initialState = cacheStore ?
+    JSON.parse(cacheStore) :
+    loadedState;
+const store = (createStore)(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const saveState = () => {
+  window.sessionStorage.setItem("redux-store", JSON.stringify(store.getState()));
+};
+
+store.subscribe(function(){console.log(store.getState())});
+store.subscribe(saveState);
 
 class App extends Component {
   render() {
     return (
-      <div >
+        <Provider store={store}>
+          <div >
       	<Navbar/>
        	<Summary/>
+              </Provider>
       </div>
     );
   }
