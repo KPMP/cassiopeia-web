56ec64db8707f75a8fe48333a21259e0936d06f0
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2018-11-28 16:37:45 -0500

    Fixed rendering on the MenuSlideList and the button on the summary page

--- src/components/Slides/MenuSlideList.js
+++ src/components/Slides/MenuSlideList.js
@@ -4,9 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretLeft, faChevronRight, faChevronLeft, faPrint, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 class MenuSlideList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.noSlidesFound = this.noSlidesFound.bind(this);
+    }
+
+    noSlidesFound() {
+        return Object.keys(this.props.selectedPatient).length === 0
+            && this.props.selectedPatient.constructor === Object;
+    }
+
     render() {
         console.log(this.props);
-    	return (
+    	return this.noSlidesFound() ? (
+            <Col id="menu-slide-list">
+                <Row className="slide-menu-item">
+                    <Col sm="12"> There are no slides to show. </Col>
+                </Row>
+            </Col>
+            ) : (
             <Col id="menu-slide-list">
             	<Row className="menu-slide-list-header">
             		<Col sm="11" >WHOLE SLIDE IMAGES</Col>
@@ -14,34 +31,33 @@ class MenuSlideList extends Component {
             			<FontAwesomeIcon icon={faCaretLeft} size="lg" onClick={this.props.onToggle} className="clickable"/>
             		</Col>
             	</Row>
-                <Row className="prev-next-buttons">
-                    <Button outline color={'secondary'}>
-                        <FontAwesomeIcon icon={faChevronLeft} size="2x"/>
-                    </Button>
-                    <Button outline color={'secondary'}>
-                        <FontAwesomeIcon icon={faDownload} size="2x"/>
-                    </Button>
-                    <Button outline color={'secondary'}>
-                        <FontAwesomeIcon icon={faPrint} size="2x"/>
-                    </Button>
-                    <Button outline color={'secondary'}>
-                        <FontAwesomeIcon icon={faChevronRight} size="2x"/>
-                    </Button>
+                <Row className="prev-next-buttons" noGutters>
+                    <Col>
+                        <Button outline color={'secondary'}>
+                            <FontAwesomeIcon icon={faDownload} size="2x"/>
+                        </Button>
+                        <Button outline color={'secondary'}>
+                            <FontAwesomeIcon icon={faPrint} size="2x"/>
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button outline color={'secondary'}>
+                            <FontAwesomeIcon icon={faChevronLeft} size="2x"/>
+                        </Button>
+                        <Button outline color={'secondary'}>
+                            <FontAwesomeIcon icon={faChevronRight} size="2x"/>
+                        </Button>
+                    </Col>
                 </Row>
             	<div id="menu-slide-list-slides">
-            	
-            	{Object.keys(this.props.selectedPatient).length === 0 && this.props.selectedPatient.constructor === Object  ? (
-            		<Row className="slide-menu-item">
-            			<Col sm="12"> There are no slides to show. </Col>
-            		</Row>
-            	) : (
-        			this.props.selectedPatient.map(function(slide, index) {
-                		return <Row className="slide-menu-item">
-                			<Col sm="2"><div className="thumbnail" /></Col>
-                			<Col sm="10" className="slide-name">{slide.slideName}</Col>
-                		</Row>;
-                	})
-            	)}
+                    {
+                        this.props.selectedPatient.map(function(slide, index) {
+                            return <Row className="slide-menu-item">
+                                <Col sm="2"><div className="thumbnail" /></Col>
+                                <Col sm="10" className="slide-name">{slide.slideName}</Col>
+                            </Row>
+                        })
+                    }
             	</div>
             	<Row className="divider" />
             	<Row>
@@ -52,7 +68,7 @@ class MenuSlideList extends Component {
                     </div>
                 </Row>
             </Col>
-        );
+        )
     }
 }
 
