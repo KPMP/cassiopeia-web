20c86869ba957cbbe2b9e4a9bf4cbb422a370a2f
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2018-11-30 17:02:58 -0500

    Zeroing in on a functioning set of CSS rules to print with

--- src/components/Slides/MenuSlideList.js
+++ src/components/Slides/MenuSlideList.js
@@ -8,11 +8,39 @@ class MenuSlideList extends Component {
     constructor(props) {
         super(props);
         this.noSlidesFound = this.noSlidesFound.bind(this);
+        this.onPrint = this.onPrint.bind(this);
     }
 
     noSlidesFound() {
         return Object.keys(this.props.selectedPatient).length === 0
             && this.props.selectedPatient.constructor === Object;
+    }
+
+    onPrint() {
+        let slideTitleText = 'Slide Title';
+        let slideTypeText = 'Slide Type';
+
+        let openseadragon = document.getElementsByClassName('openseadragon')[0];
+        let canvasDiv = openseadragon.getElementsByClassName('openseadragon-canvas')[0];
+        let canvas = canvasDiv.getElementsByTagName('canvas')[0];
+        let imageContainer = document.createElement('img');
+        let slideTitle = document.createElement('h1');
+        let slideType = document.createElement ('h2');
+        slideTitle.setAttribute('id', 'print-slide-title');
+        slideType.setAttribute('id', 'print-slide-type');
+        slideTitle.innerHTML = slideTitleText;
+        slideType.innerHTML = slideTypeText;
+
+        imageContainer.setAttribute('id', 'print-slide-image');
+        imageContainer.setAttribute('src', canvas.toDataURL('image/jpeg'));
+
+        document.body.appendChild(imageContainer);
+        document.body.appendChild(slideTitle);
+        document.body.appendChild(slideType);
+        window.print();
+        document.body.removeChild(imageContainer);
+        document.body.removeChild(slideTitle);
+        document.body.removeChild(slideType);
     }
 
     render() {
@@ -35,8 +63,8 @@ class MenuSlideList extends Component {
                         <Button outline color={'secondary'}>
                             <FontAwesomeIcon icon={faDownload} size="2x"/>
                         </Button>
-                        <Button outline color={'secondary'}>
-                            <FontAwesomeIcon icon={faPrint} size="2x"/>
+                        <Button outline color={'secondary'} onClick={this.onPrint}>
+                            <FontAwesomeIcon icon={faPrint} size="2x" />
                         </Button>
                     </Col>
                     <Col>
@@ -60,7 +88,7 @@ class MenuSlideList extends Component {
             	</div>
             	<Row className="divider" />
             	<Row>
-                    <div id="slide-stain-text">
+                    <div id="menu-slide-stain-text" class="slide-stain-text">
                         <h2>Slide Title</h2>
                         <h3>Slide Stain Name</h3>
                         <p>Slide Stain Flava Text</p>
