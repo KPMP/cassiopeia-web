67968fccd1f4e39c3a06e363c8ca39f6fd3e2bf8
Becky Reamy <rlreamy@med.umich.edu>
2018-11-30 14:27:42 -0500

    KPMP-446: Added tests around downloadSlide

---
+++ src/components/Slides/downloadSlide.test.js
@@ -0,0 +1,64 @@
+import { downloadSlide } from './downloadSlide';
+
+describe("downloadSlide", () => {
+	beforeEach(() => {
+		document.body.innerHTML = 
+			 '<a id="download" download={downloadFileName}><FontAwesomeIcon icon={faDownload} size="2x" className="clickable"/></a>' +
+			 '<div class="openseadragon"><div class="openseadragon-canvas">' +
+				'<canvas id="myCanvas" width="500" height="500"></canvas>'+
+			 '</div></div><div class="openseadragon">second one for navigator pane</div>';
+		const window = document.defaultView;
+		mockCanvas(window);
+	});
+	
+	it("", () => {
+		var canvas = document.getElementById("myCanvas");
+		var context = canvas.getContext('2d');
+		
+		downloadSlide();
+
+		let result = document.getElementById("download");
+		expect(result.href).toEqual('http://localhost/stuff');
+	});
+});
+
+const mockCanvas = (window) => {
+    window.HTMLCanvasElement.prototype.getContext = function () {
+        return {
+            fillRect: function() {},
+            clearRect: function(){},
+            getImageData: function(x, y, w, h) {
+                return  {
+                    data: new Array(w*h*4)
+                };
+            },
+            putImageData: function() {},
+            createImageData: function(){ return []},
+            setTransform: function(){},
+            drawImage: function(){},
+            save: function(){},
+            fillText: function(){},
+            restore: function(){},
+            beginPath: function(){},
+            moveTo: function(){},
+            lineTo: function(){},
+            closePath: function(){},
+            stroke: function(){},
+            translate: function(){},
+            scale: function(){},
+            rotate: function(){},
+            arc: function(){},
+            fill: function(){},
+            measureText: function(){
+                return { width: 0 };
+            },
+            transform: function(){},
+            rect: function(){},
+            clip: function(){},
+        };
+    }
+
+    window.HTMLCanvasElement.prototype.toDataURL = function () {
+        return "stuff";
+    }
+}
