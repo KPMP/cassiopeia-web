449f735d193869065057cd9da8bd9d61799ad913
Becky Reamy <rlreamy@umich.edu>
2018-12-07 10:07:32 -0500

    KPMP-501: Finish up with styling

--- src/components/Slides/BurgerMenu.js
+++ src/components/Slides/BurgerMenu.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import MenuSlideListContainer from './MenuSlideListContainer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
-
+import { faBars, faCaretLeft, faHome } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 class BurgerMenu extends Component {
 	
 	render() {
 		return (
-			<Menu isOpen={ true } width={ '450px' } noOverlay customBurgerIcon={ <FontAwesomeIcon icon={faBars} size="2x"/> }
-				customCrossIcon={ <FontAwesomeIcon icon={faCaretLeft} size="lg"/>} >
-				<MenuSlideListContainer />
-			</Menu>
+			<div>
+				<Menu isOpen={ true } width={ '450px' } noOverlay customBurgerIcon={ <FontAwesomeIcon icon={faBars} /> }
+					customCrossIcon={ <FontAwesomeIcon icon={faCaretLeft} size="lg"/>} >
+					<MenuSlideListContainer />
+				</Menu>
+                <Link id="btn-home" to="/">
+                    <FontAwesomeIcon icon={faHome} size="2x"/>
+                </Link>
+            </div>
 		);
 	}
 	
