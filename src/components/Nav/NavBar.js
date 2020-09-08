2387c436fd65b5ae97dbb2b802ab8b7464b85e99
Zach Wright <zwright@umich.edu>
2020-09-08 15:29:13 -0400

    KPMP-2069: Fix margins

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Row, NavbarBrand, Container } from 'reactstrap';
+import { Row, NavbarBrand } from 'reactstrap';
 
 class NavBar extends Component {
 	render() {
 		return (
-			<Container fluid>
 			<Row className="nav-container container-fluid">
 				<NavbarBrand href="/">
 					<img src="img/logo.png" alt="Participant Slide Viewer" className="logo"/>
 				</NavbarBrand>
 			</Row>
-			</Container>
 		);
 	}
 }
