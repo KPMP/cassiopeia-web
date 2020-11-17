7d651999e25b185607c1359d2cd67679509febb9
Zach Wright <zwright@umich.edu>
2020-11-17 10:27:11 -0500

    KPMP-2244: Add 404 image

--- src/components/Error/NotFound.js
+++ src/components/Error/NotFound.js
@@ -6,11 +6,11 @@ class NotFound extends Component {
         return (
             <article className="container-fluid">
                 <Row id="error-content">
-                    <Col xs={0} md={2} lg={4}>&nbsp;</Col>
-                    <Col xs={12} md={4} lg={2}>
-                        <p className="error-huge align-top">404</p>
+                    <Col md={2} lg={4}>&nbsp;</Col>
+                    <Col md={4} lg={2} className={"ml-auto"}>
+                        <img src="img/404.svg" alt="Page not found" id="oops-image"/>
                     </Col>
-                    <Col xs={12} md={6} lg={6} className="not-found-text">
+                    <Col md={6} lg={6} className="not-found-text">
                         <p className="error-small">Sorry. We couldn't find the <br/>
                             page you're looking for.</p>
                         <p>If you're still having problems, please contact <a href="mailto:admin@kpmp.org">admin@kpmp.org</a></p>
