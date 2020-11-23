98b7f5cb043510efeca620d81c924653ff3dadb2
Zach Wright <zwright@umich.edu>
2020-11-23 14:21:48 -0500

    KPMP-2244: Add import

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import loadedState from './initialState';
 import rootReducer from './reducers';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import SlidePrintManager from './components/Slides/Menu/SlidePrintManager';
 import ReactGA from 'react-ga';
 import createHistory from 'history/createBrowserHistory';
