827496a7e239416f51e2357d4cd6aa1f0ef2e715
Becky Reamy <rlreamy@umich.edu>
2021-04-21 10:44:40 -0400

    KPMP-2556 & KPMP-2629: Fix sign out and add link to home page

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import { NavbarBrand, Button } from 'reactstrap';
+import { NavbarBrand, Button } from 'reactstrap'
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
 
 	render() {
 		return (
-			<nav id="navbar" className="nav-container fixed-top navbar navbar-expand-* px-4 py-2">
-				<NavbarBrand className="d-flex align-items-center ml-3">
-					<img src="img/logo.png" className="logo" alt="Participant Slide Viewer"/>
-					<span id="title-text" className="ml-4">Participant Portal</span>
-				</NavbarBrand>
-				<ul className="navbar-nav">
-						<li className="nav-item small">
-							<Button color="link" onClick={() => window.location.href="/about"}>About</Button> &nbsp;|&nbsp; 
-							<Button color="link" onClick={() => window.location.href="https://login.dev-mydata.kpmp.org/idp/Shibboleth.sso/Logout"}>Sign Out</Button>
+			<nav id='navbar' className='nav-container fixed-top navbar navbar-expand-* px-4 py-2'>
+				<Link to='/' className='navbar-header'>
+					<NavbarBrand className='d-flex align-items-center ml-3'>
+						<img src='img/logo.png' className='logo' alt='Participant Slide Viewer'/>
+						<span id='title-text' className='ml-4'>Participant Portal</span>
+					</NavbarBrand>
+				</Link>
+				<ul className='navbar-nav'>
+						<li className='nav-item small'>
+							<Button color='link' onClick={() => window.location.href='/about'}>About</Button> &nbsp;|&nbsp; 
+							<Button color='link' onClick={() => window.location.href='https://dev-mydata.kpmp.org/Shiboleth.sso/Logout?return=https://login.dev-mydata.kpmp.org/idp/profile/Logout'}>Sign Out</Button>
 						</li>
 				</ul>
 			</nav>
