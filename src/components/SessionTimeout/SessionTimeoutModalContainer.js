ffcb035f72ca86160acc899c8c5df721e6563d85
Becky Reamy <rlreamy@umich.edu>
2021-05-19 16:30:25 -0400

    KPMP-2651: Timeout after 5 minutes

---
+++ src/components/SessionTimeout/SessionTimeoutModalContainer.js
@@ -0,0 +1,29 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import SessionTimeoutModal from './SessionTimeoutModal';
+import { sessionTimedOut } from '../../actions/SessionTimeout/sessionTimeoutAction';
+
+const mapStateToProps = (state, props) =>
+    ({
+        sessionTimedOut: state.sessionTimedOut
+    });
+
+const mapDispatchToProps = (dispatch, props) =>
+    ({
+        startTimer(){
+            startTimer(dispatch);
+        },
+        restartTimer() {
+            dispatch(sessionTimedOut(false));
+            startTimer(dispatch);
+        }
+    });
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SessionTimeoutModal));
+
+export const startTimer = (dispatch) => {
+    clearTimeout();
+    setTimeout(function() {
+        dispatch(sessionTimedOut(true));
+    }, 5 * 60 * 1000);
+}
