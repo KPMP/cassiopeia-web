649ee64f72eee334be707fa52ea0e9a82557cac1
Zach Wright <zwright@umich.edu>
2022-06-15 15:32:59 -0400

    KPMP-3646: Extra code

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = (props) => {
 		  <NavbarToggler onClick={toggle} />
 		  <Collapse isOpen={isOpen} navbar>
 			<Nav className="ml-auto" navbar>
-				<NavItem className={`${window.location.pathname.replaceAll("/", "") === 'help' ? 'active' : ''} px-1`}>
+				<NavItem className='px-1'>
 					<NavLink href='https://www.kpmp.org/participant-letters' target='_blank'>Participant letters</NavLink>
 				</NavItem>
 				<NavItem className={`${window.location.pathname.replaceAll("/", "") === 'help' ? 'active' : ''} px-1`}>
