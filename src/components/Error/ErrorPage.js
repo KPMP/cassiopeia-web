90718a8637386839312140fe2ee4b01c78549dcd
dert1129 <nathancreger1111@gmail.com>
2022-08-26 15:38:40 -0400

    Added dizzy kidney to oops page

--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -4,13 +4,16 @@ import { Col, Row, Button } from 'reactstrap';
 class ErrorPage extends Component {
     render() {
         return (
-            <article className="container-fluid" id="error-container">
-                <Row id="error-content">
+            <article className="container-fluid">
+                <Row id="oops-content">
                     <Col xs={0} md={2}>&nbsp;</Col>
+                    <Col xs={12} md={4} className={"text-center"}>
+                        <img src="img/oops.png" alt="Oops, something went wrong" id="oops-image"/>
+                    </Col>
                     <Col xs={12} md={6}>
-                        <p className="error-big">Oops...</p>
-                        <p className="error-small">Looks like something went wrong.<br/>We&#39;re working on it.</p>
-                        <p className="error-button-container">
+                        <p className="oops-big">Oops...</p>
+                        <p className="oops-small">Looks like something went wrong.<br/>We&#39;re working on it.</p>
+                        <p className="oops-button-container">
                             <Button className="btn btn-primary"
                                     onClick={() => window.location.href = "/"}>
                                     Back to Home
